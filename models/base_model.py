79de8cd545268e70c09cf50b3d7a53d7ef90a262
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-01 14:48:36 -0800

    BaseModel Class

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#BaseModel class definition
+import uuid
+import datetime
+
+class BaseModel:
+    #Basemodel class
+    def __init__(self):
+        #Initialize the object
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        #print string representation of the class
+        print(f"[{self.__class__.__name__}] ({self.id}) self.__dict__")
+
+    def save(self):
+        #update the object
+        self.updated_at = datetime.datetime.now()
+    
+    def to_dict(self):
+        #returns dictionary representation of an object
+        the_dict = self.__dict__
+        the_dict["__class__"] = self.__class__.__name__
+        the_dict["created_at"] = the_dict["created_at"].isoformat(sep="T", timespec="auto")
+        the_dict["updated_at"] = the_dict["updated_at"].isoformat(sep="T", timespec="auto")
+        return the_dict
