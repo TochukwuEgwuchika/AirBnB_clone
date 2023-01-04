34bd831d3f1a72bf49ca1b3ce08f06a3b6c2bf76
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-04 14:18:25 -0800

    a

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,14 @@
 #BaseModel class definition
 import uuid
 import datetime
-from __init__ import storage
+
+
+import sys, os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+#parentdir = os.path.dirname(parentdir)
+sys.path.insert(0, parentdir)
+from models.__init__ import storage
 
 class BaseModel:
     #Basemodel class
