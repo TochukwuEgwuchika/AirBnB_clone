ca3b8466df21931da3cc1d5f625951acb54f0c66
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-04 13:45:53 -0800

    __objects variable fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,20 +8,20 @@ class FileStorage:
 
     def all(self):
         #returns the dictionary __objects
-        return __objects
+        return self.__class__.__objects
     
     def new(self, obj):
         #sets in __objects the obj with key <obj class name>.id
-        __objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        self.__class__.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
         #serializes __objects to the JSON file (path: __file_path)
         with open(__file_path, "w") as f:
-            json.dump(f, __objects)
+            json.dump(f, self.__class__.__objects)
 
     def reload(self):
         #deserializes the JSON file to __objects (only if the JSON file (__file_path)
         try:
-            __objects = json.load(__file_path)
+            self.__class__.__objects = json.load(__file_path)
         except:
             pass
