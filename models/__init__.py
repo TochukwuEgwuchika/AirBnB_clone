1d3c14161ce3e949cebb0f5f0afb79ed8c15cb0e
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-04 13:41:14 -0800

    Parent directory appended

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,11 @@
-from engine import file_storage
+import sys, os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+#parentdir = os.path.dirname(parentdir)
+sys.path.insert(0, parentdir)
+
+
+from models.engine import file_storage
 
 storage = file_storage.FileStorage()
 storage.reload()
