fcd9e5f3f36406bbc4db3c3ab6ede89d68984f4c
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-06 03:18:49 -0800

    AIRBNB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,14 @@
 #The definition of the file storage
 import json
 
+import sys, os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)
+sys.path.insert(0, parentdir)
+
+from models.base_model import BaseModel
+
 class FileStorage:
     #The file storage class
     __file_path = "file.json"
@@ -26,7 +34,10 @@ class FileStorage:
 
     def reload(self):
         #deserializes the JSON file to __objects (only if the JSON file (__file_path)
-        try:
-            self.__class__.__objects = json.load(self.__class__.__file_path)
-        except:
-            pass
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r") as f:
+            objects = json.loads(f)
+            FileStorage.__objects = {}
+            for key in objects:
+                FileStorage.__objects[key] = BaseModel(**objects[key])
