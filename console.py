eeb89c764f200029a515f01cf13cd289b6599e52
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-11 14:01:01 -0800

    console.py added

---
+++ console.py
@@ -0,0 +1,20 @@
+#contains the entry point of the command interpreter
+
+class HBNBCommand(cmd.Cmd):
+    #console class
+    prompt = "(hbnb)"
+
+    def do_quit(self):
+        #quit when quit is entered
+        return
+
+    def do_EOF(self):
+        #quit when EOF is entered
+        return
+
+    def emptyline(self):
+        #when an empty line is entered
+        pass
+
+    if __name__ == '__main__':
+        HBNBCommand().cmdloop()
