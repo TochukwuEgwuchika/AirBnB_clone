8e9787bd33a385143c9db6111f0504559b01dec0
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-11 14:22:40 -0800

    console.py added

--- console.py
+++ console.py
@@ -2,22 +2,22 @@
 
 import cmd
 
-#contains the entry point of the command interpreter
+"""contains the entry point of the command interpreter"""
 
 class HBNBCommand(cmd.Cmd):
-    #console class
+    """implementation of the cmd class"""
     prompt = "(hbnb)"
 
     def do_quit(self, s):
-        #quit when quit is entered
+        """quit when quit is entered"""
         return True
 
     def do_EOF(self, s):
-        #quit when EOF is entered
+        """quit when EOF is entered"""
         return True
 
     def emptyline(self):
-        #when an empty line is entered
+        """when an empty line is entered"""
         pass
 
 if __name__ == '__main__':
