1d494afdfa0441c8cc13fc76128f0e13a905bd97
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-11 13:09:43 -0800

    done

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,13 @@
+import unittest
+#test all methods
+
+class TestFileStorage(unittest.TestCase):
+    #test all methods
+    def test_reload():
+        pass
+    def test_new():
+        pass
+    def test_save():
+        pass
+    def test_all():
+        pass
