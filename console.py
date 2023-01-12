6864314990347bdb3b233461f932f4b3d91a3e5f
Tochukwu Egwuchika <tobezonyeze@gmail.com>
2023-01-12 04:53:18 -0800

    DOne

--- console.py
+++ console.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+"""
+console base for the application
+"""
 import cmd
-
-"""contains the entry point of the command interpreter"""
 
 class HBNBCommand(cmd.Cmd):
     """implementation of the cmd class"""
